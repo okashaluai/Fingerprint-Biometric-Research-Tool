c80149143c849ec3fe1c570f78385c82c01da46d
Luai Okasha <luaiokasha@gmail.com>
2024-05-17 17:03:55 +0300

    Asset class fields were defined and to_dto() method was implemented.

--- Dev/LogicLayer/LogicObjects/Asset.py
+++ Dev/LogicLayer/LogicObjects/Asset.py
@@ -1,18 +1,18 @@
 from abc import abstractmethod
-
 from Dev.DTOs import AssetDTO
 from Dev.DataAccessLayer.DAOs import AssetDAO
 from Dev.LogicLayer.LogicObjects.ILogicObject import ILogicObject
-
+from datetime import datetime
 
 class Asset(ILogicObject):
-    def __init__(self):
-        pass
+    def __init__(self, path):
+        self.__path = path
+        self.__date = datetime.now()
 
     @abstractmethod
     def to_dto(self) -> AssetDTO:
-        raise NotImplementedError
+        return AssetDTO(0, self.__path, self.__date)
 
     @abstractmethod
     def to_dao(self) -> AssetDAO:
-        raise NotImplementedError
+        return AssetDAO()
