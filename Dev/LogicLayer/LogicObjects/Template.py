17cd16245d177d1c47491726f0ea8bd7a20cec4a
Luai Okasha <luaiokasha@gmail.com>
2024-05-17 22:01:30 +0300

    equal method was implemented to check if two templates files are equal based on their .min and .xyt sorted content

--- Dev/LogicLayer/LogicObjects/Template.py
+++ Dev/LogicLayer/LogicObjects/Template.py
@@ -2,14 +2,34 @@ from Dev.DTOs import TemplateDTO
 from Dev.DataAccessLayer.DAOs import TemplateDAO
 from Dev.LogicLayer.LogicObjects.Asset import Asset
 from Dev.LogicLayer.LogicObjects.ILogicObject import ILogicObject
-
+import os
 
 class Template(Asset):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, path):
+        super().__init__(path)
 
     def to_dto(self) -> TemplateDTO:
         raise NotImplementedError
 
     def to_dao(self) -> TemplateDAO:
         raise NotImplementedError
+
+    def equal(self, other : object) -> bool:
+        f1_min_content = []
+        f1_xyt_content = []
+        f2_min_content = []
+        f2_xyt_content = []
+        
+        with open(os.path.join(self.__path, '.min')) as f:
+            f1_min_content = f.readlines()
+
+        with open(os.path.join(self.__path, '.xyt')) as f:
+            f1_xyt_content = f.readlines()
+        
+        with open(os.path.join(other.__path, '.min')) as f:
+            f2_min_content = f.readlines()
+
+        with open(os.path.join(other.__path, '.xyt')) as f:
+            f2_xyt_content = f.readlines()
+
+        return (f1_min_content.sort() == f2_min_content.sort()) and (f1_xyt_content.sort() == f2_xyt_content.sort())
