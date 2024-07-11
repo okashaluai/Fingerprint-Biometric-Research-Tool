8ecf8d49ad6b46be980284c58fdc69156662f762
Luai Okasha <luaiokasha@gmail.com>
2024-07-12 01:58:26 +0300

    Asset date was removed.

--- Dev/LogicLayer/LogicObjects/PrintingObject.py
+++ Dev/LogicLayer/LogicObjects/PrintingObject.py
@@ -9,13 +9,7 @@ class PrintingObject(Asset):
         super().__init__(path, is_dir)
 
     def to_dto(self) -> PrintingObjectDTO:
-        return PrintingObjectDTO(path=self.path, date=self.date, is_dir=self.is_dir)
+        return PrintingObjectDTO(path=self.path, is_dir=self.is_dir)
 
     def to_dao(self) -> PrintingObjectDAO:
         raise NotImplementedError
-
-    def finalize_path(self, final_destination_path: str):
-        if os.path.exists(final_destination_path) and os.path.isdir(final_destination_path):
-            super().path = final_destination_path
-        else:
-            raise Exception(f'Final Destination was not found {final_destination_path} does not exist')
