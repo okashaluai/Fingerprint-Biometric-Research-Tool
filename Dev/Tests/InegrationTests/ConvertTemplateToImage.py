89f2d274f6784b4701989f9df607afa6c13ee86d
Yazan Abbas <yazan.ahmad.abass@gmail.com>
2024-07-13 18:13:45 +0300

    add multi convert templates to images

--- Dev/Tests/InegrationTests/ConvertTemplateToImage.py
+++ Dev/Tests/InegrationTests/ConvertTemplateToImage.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from pathlib import Path
 
 from Dev.DTOs import ImageDTO, TemplateDTO, ExperimentDTO
 from Dev.LogicLayer.Service.Service import Service
@@ -51,6 +52,29 @@ class ConvertTemplateToImage(unittest.TestCase):
 
         # assert os.path.exists(generated_image.path)
         # assert generated_image.path.endswith('109_1_8bit.png')
+
+    def test_convert_many_templates_to_many_images(self):
+        valid_template = TemplateDTO(
+            path=os.path.join(templates_path, 'many_templates_for_convert'),
+            is_dir=True
+        )
+
+        response = self.service.convert_template_to_image(valid_template)
+        assert response.success
+        assert response.data is not None
+        generated_image: ImageDTO = response.data
+
+        assert generated_image.path != ""
+        assert os.path.exists(generated_image.path)
+
+        files_counter = 0
+        for t in os.listdir(valid_template.path):
+            base_name = Path(t).stem
+            for im in os.listdir(generated_image.path):
+                if im.startswith(base_name):
+                    files_counter += 1
+
+        assert files_counter >= len(os.listdir(valid_template.path))
 
     def test_convert_invalid_template_to_image(self):
         invalid_template = TemplateDTO(
