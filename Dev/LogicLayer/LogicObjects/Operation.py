5feab4df0f6396897bd54cf38352286b02704a06
Luai Okasha <luaiokasha@gmail.com>
2024-07-22 02:21:30 +0300

    Operation and OperationDTO field has been fixed to be compatible with matching operation.

--- Dev/LogicLayer/LogicObjects/Operation.py
+++ Dev/LogicLayer/LogicObjects/Operation.py
@@ -7,18 +7,18 @@ from Dev.LogicLayer.LogicObjects.ILogicObject import ILogicObject
 
 class Operation(ILogicObject):
     def __init__(self, operation_id: str, operation_type: OperationType, operation_input: Asset,
-                 operation_output: Asset, operation_datetime: datetime, operation_optional_extra_input: Asset = None):
+                 operation_output: Asset | str, operation_datetime: datetime, operation_optional_extra_input: Asset | str = ''):
         self.operation_id: str = operation_id
         self.operation_type: OperationType = operation_type
         self.operation_input: Asset = operation_input
-        self.operation_optional_extra_input: Asset = operation_optional_extra_input
-        self.operation_output: Asset = operation_output
+        self.operation_optional_extra_input: Asset | str = operation_optional_extra_input
+        self.operation_output: Asset | str = operation_output
         self.operation_datetime: datetime = operation_datetime
 
     def to_dto(self) -> OperationDTO:
         return OperationDTO(operation_id=self.operation_id,
                             operation_type=self.operation_type,
                             operation_input=self.operation_input.to_dto(),
-                            operation_output=self.operation_output.to_dto(),
+                            operation_output=self.operation_output.to_dto() if not isinstance(self.operation_output, str) else self.operation_output,
                             operation_datetime=self.operation_datetime,
-                            operation_optional_extra_input=self.operation_optional_extra_input.to_dto())
+                            operation_optional_extra_input= self.operation_optional_extra_input.to_dto() if not isinstance(self.operation_optional_extra_input, str) else self.operation_optional_extra_input)
