bdef3ae457f9562d88c8376a87da63ebe8acf0f7
Luai Okasha <luaiokasha@gmail.com>
2024-07-22 03:37:51 +0300

    fixed a bug related to default current experiment on loading the system.

--- Dev/DataAccessLayer/FILESYSTEM.py
+++ Dev/DataAccessLayer/FILESYSTEM.py
@@ -24,9 +24,10 @@ class FILESYSTEM(metaclass=Singleton):
         for experiment_dir in experiments_dirs:
             try:
                 experiment_dto = self.load_experiment(experiment_dir)
-            except Exception:
-                pass
-            experiments.append(experiment_dto)
+                experiments.append(experiment_dto)
+
+            except Exception as error:
+                print(f'error loading experiment: {error}')
 
         # if not (os.path.exists(self.temp_dir) and os.path.isdir(self.temp_dir)):
         #     os.mkdir(self.temp_dir)
@@ -40,37 +41,52 @@ class FILESYSTEM(metaclass=Singleton):
         operations_dirs = os.listdir(experiment_path)
 
         operations: list[OperationDTO] = list()
-        for operation_dir in operations_dirs:
-            operation_dto = self.load_operation(experiment_name, operation_dir)
-            operations.append(operation_dto)
-        # creation_date = os.stat(experiment_path).st_ctime
+
         latest_update_date = datetime.fromtimestamp(os.stat(experiment_path).st_mtime)
+
+        for operation_dir in operations_dirs:
+
+            operation_dto = self.load_operation(experiment_name, operation_dir)
+
+            if operation_dto is None:
+                continue
+            if latest_update_date < operation_dto.operation_datetime:
+                latest_update_date = operation_dto.operation_datetime
+            operations.append(operation_dto)
+        creation_date = datetime.fromtimestamp(os.stat(experiment_path).st_ctime)
         experiment_dto = ExperimentDTO(experiment_name=experiment_name,
-                                       experiment_datetime=latest_update_date,
+                                       experiment_datetime=creation_date,
+                                       experiment_last_update_date = latest_update_date,
                                        operations=operations)
         return experiment_dto
 
-    def load_operation(self, experiment_name: str, operation_id: str) -> OperationDTO:
-        operation_path = os.path.join(self.experiments_home_path, experiment_name, operation_id)
-        if not os.path.isdir(operation_path):
-            raise Exception(f'Operation {operation_path} does not exist')
+    def load_operation(self, experiment_name: str, operation_id: str) -> OperationDTO|None:
+        try:
+            operation_path = os.path.join(self.experiments_home_path, experiment_name, operation_id)
+            if not os.path.isdir(operation_path):
+                raise Exception(f'Operation {operation_path} does not exist')
 
-        operation_metadata = self.load_operation_metadata(experiment_name, operation_id)
-        operation_type = OperationType(operation_metadata['operation_type'])
-        operation_datetime = datetime.fromtimestamp(float(operation_metadata['operation_timestamp']))
-        operation_input = self.wrap_asset(operation_metadata['input_asset_path'])
-        operation_output = self.wrap_asset(operation_metadata['output_asset_path'])
-        operation_extra_optional_input = self.wrap_asset(operation_metadata['optional_extra_asset_path'])
+            operation_metadata = self.load_operation_metadata(experiment_name, operation_id)
+            operation_type = OperationType(operation_metadata['operation_type'])
+            operation_datetime = datetime.fromtimestamp(float(operation_metadata['operation_timestamp']))
+            operation_input = self.wrap_asset(operation_metadata['input_asset_path'])
+            operation_output = self.wrap_asset(operation_metadata['output_asset_path'])
+            operation_extra_optional_input = self.wrap_asset(operation_metadata['optional_extra_asset_path'])
 
-        operation_dto = OperationDTO(operation_id=operation_id,
-                                     operation_datetime=operation_datetime,
-                                     operation_type=operation_type,
-                                     operation_input=operation_input,
-                                     operation_output=operation_output,
-                                     operation_optional_extra_input=operation_extra_optional_input)
-        return operation_dto
+            operation_dto = OperationDTO(operation_id=operation_id,
+                                         operation_datetime=operation_datetime,
+                                         operation_type=operation_type,
+                                         operation_input=operation_input,
+                                         operation_output=operation_output,
+                                         operation_optional_extra_input=operation_extra_optional_input)
+            return operation_dto
+        except Exception as error:
+            print(f'Error loading Operation {operation_id} of Experiment {experiment_name}: {error}')
+            return None
 
     def wrap_asset(self, asset_path: str) -> AssetDTO | str:
+        if asset_path == '':
+            return  ''
         if not os.path.exists(asset_path):
             raise Exception(f'Asset {asset_path} path does not exist')
 
@@ -118,7 +134,7 @@ class FILESYSTEM(metaclass=Singleton):
 
     def delete_operation_dir(self, experiment_name: str, operation_id: str):
         operation_dir_path = os.path.join(self.experiments_home_path, experiment_name, operation_id)
-        if os.path.isdir(operation_dir_path):
+        if os.path.exists(operation_dir_path):
             shutil.rmtree(operation_dir_path)
 
     def prepare_template_to_image_operation_dir(self, experiment_name: str, operation_id: str):
